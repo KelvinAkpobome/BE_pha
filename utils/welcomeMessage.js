e02a33cc5b16fa74e245f41912a41bebcd46d465
kelvinakpobome <kelvinakpobome@gmail.com>
2021-11-11 00:05:44 +0100

    Fixes: fix for welcome page

--- utils/welcomeMessage.js
+++ utils/welcomeMessage.js
@@ -6,12 +6,7 @@ const message = (url) => {
   const html = `
           <body style="margin: 0; padding: 0;">
           <h1>You are welcome to Port Harcourt Agents API</h1>
-            <p>Home Hunting Made Easy in Port Harcourt, kindly see the link below for usage</p>
-                <a style="width:250px; display:inline-block; text-decoration: none; font-size: 15px; text-align: center;
-                  background-color:#55acee; border-radius:2px; color:white; height:32px; cursor: pointer; padding-top:5px"
-                          href=${url}>
-                          Documentation here
-                        </a>
+            <p>Home Hunting Made Easy in Port Harcourt, see the docs <a href ="${url}">here</a></p>
           </body>`;
   return html;
 };
